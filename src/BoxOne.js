e5b424e3edea222a81e4593d0404d9027f5f7cf6
ZhangYuP <zyp5318@qq.com>
2019-07-02 19:37:44 +0800

    v1

---
+++ src/BoxOne.js
@@ -0,0 +1,12 @@
+import React,{ Component } from 'react';
+import './BoxOne.css'
+
+export default class extends Component {
+  render() {
+    return (
+      <div className="BoxOne">
+        <div> Box One </div>
+      </div>
+    )
+  }
+}  
